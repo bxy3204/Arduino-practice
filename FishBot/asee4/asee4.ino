e4992cd74c757a64a045d4b99a025bb2b5d351c8
Brian Youngman <byoungman12@gmail.com>
2015-05-13 20:22:28 -0400

    sfdf
    sdfed

--- FishBot/asee4/asee4.ino
+++ FishBot/asee4/asee4.ino
@@ -24,17 +24,11 @@ int camFlag = 0;
 int flag1 = 0;
 int leftSpeed = 70;
 int rightSpeed = 70;
-int grabFlag=0;
+int grabFlag = 0;
 #include <Servo.h>
- int grabPos=0;
- int dropFlag =0;
+int grabPos = 0;
+int dropFlag = 0;
 Servo myservo;  // create servo object to control a servo
-                // a maximum of eight servo objects can be created
- 
-
- 
-
-
 uint16_t blocks; //holds number of objects found
 int setCam = 0; // check if camera is set to initial position
 int fish = 0;  //number of fish caught
@@ -66,62 +60,52 @@ void setup() {
   digitalWrite(Dir2, LOW);
   Serial.begin(9600);
   Serial.println("start");
-  
   myservo.attach(9);
   myservo.write(140);
-  pinMode(24, OUTPUT);     
-
+  pinMode(24, OUTPUT);
   pinMode(25, OUTPUT);
-
   digitalWrite(24, LOW);
-
   digitalWrite(25, LOW); //Low towards bucket
-  int arm=0;
+  int arm = 0;
   delay(3000);
 }
 
 void loop() {
- readPixy();
+  readPixy();
+  if (dropFlag == 0)
+  {
+    liftDown();
+  }
+  if (driveFlag < 1 && dropFlag > 0) {
+    camAppr();
+  }
+  /*if(driveFlag==1 && fish ==0)
+  {
+    //Serial.println("grab");
+    grab(90);
+    arm=0;
+    time = millis();
+  }
+  if(grabFlag ==1 && arm !=1350 && ( millis()-time) > 500)
+  {
+    liftUp();
+    fish++;
+  }
+  if(grabFlag==1 && arm == 1350 && driveFlag <2)
+  {
+    grab(135);
 
-if(dropFlag == 0)
-{
-  
-  
-  liftDown();
-}
-if(driveFlag < 1 && dropFlag > 0){
- fwd(2100);
- 
- }
- 
+  }
+  if(grabFlag ==2 && driveFlag <2)
+  {
+    fwd(7500);
+  }
 
- if(driveFlag==1 && fish ==0)
- {
-   //Serial.println("grab");
-   grab(90);
-   arm=0;
-   time = millis();
- }
- if(grabFlag ==1 && arm !=1350 && ( millis()-time) > 500)
- {
-   liftUp();
-   fish++;
- }
- if(grabFlag==1 && arm == 1350 && driveFlag <2)
- {
-   grab(135);
-   
- }
- if(grabFlag ==2 && driveFlag <2)
- {
-   fwd(7500);
- }
- 
-/* if(driveFlag == 1)
- {
-   turn(2500);
- }*/
- 
+  /* if(driveFlag == 1)
+  {
+    turn(2500);
+  }*/
+
 }
 
 void fwd(long distance)
@@ -147,16 +131,16 @@ void fwd(long distance)
     driveFlag++;
     x = 0;
   }
- /* forward();
-  if (xLoc > 150)
-  {
-    leftSpeed=0;
-  }
-  if(xLoc < 160)
-  {
-    leftSpeed=80;
-  }*/
- 
+  /* forward();
+   if (xLoc > 150)
+   {
+     leftSpeed=0;
+   }
+   if(xLoc < 160)
+   {
+     leftSpeed=80;
+   }*/
+
 }
 
 void neutral()
@@ -219,68 +203,67 @@ void readPixy()
 //uint16_t blocks;
 
 {
+  blocks = pixy.getBlocks();
 
-   blocks = pixy.getBlocks();
+  if (!blocks)           // if no data
 
-   if(!blocks)            // if no data
+  {
 
-   {
+    if (pixyReadTimer < millis())   // and if we are beyond holding the last valid reading
 
-     if (pixyReadTimer < millis())   // and if we are beyond holding the last valid reading
+    { // then there really is nothing being seen
 
-     {                               // then there really is nothing being seen
-
-       //xLoc = 0;
+      //xLoc = 0;
 
       // yLoc = 0;
 
-       //width = 0;
+      //width = 0;
 
-       //height = 0;
+      //height = 0;
 
-       //color = 0;
+      //color = 0;
 
-     }
+    }
 
-   }
+  }
 
-   else  // Some good info from the camera
+  else  // Some good info from the camera
 
-   {
+  {
 
-      xLoc = pixy.blocks[0].x;
+    xLoc = pixy.blocks[0].x;
 
-      yLoc = pixy.blocks[0].y;
+    yLoc = pixy.blocks[0].y;
 
-      width = pixy.blocks[0].width;
+    width = pixy.blocks[0].width;
 
-      height = pixy.blocks[0].height;
+    height = pixy.blocks[0].height;
 
-      color = pixy.blocks[0].signature;
+    color = pixy.blocks[0].signature;
 
-      pixyReadTimer = millis() + 25;  // Max time to hold an old value is 25 ms.
+    pixyReadTimer = millis() + 25;  // Max time to hold an old value is 25 ms.
 
-   }
+  }
 }
 void camAppr()
 {
   area = width + height;
- if(area < 180 && area > 0){
-   forward();
- }
- if(area > 180)
- {
-   time = millis();
-   neutral();
-   fish++;
- }
+  if (area < 180 && area > 0) {
+    forward();
+  }
+  if (area > 180)
+  {
+    time = millis();
+    neutral();
+    fish++;
+  }
 }
 void forward()
 {
   digitalWrite(Dir1, HIGH);
-    digitalWrite(Dir2, HIGH);
-    analogWrite(motorLeft, leftSpeed);
-    analogWrite(motorRight, rightSpeed);
+  digitalWrite(Dir2, HIGH);
+  analogWrite(motorLeft, leftSpeed);
+  analogWrite(motorRight, rightSpeed);
 }
 void grab(int pos)
 {
@@ -290,31 +273,31 @@ void grab(int pos)
 void liftUp()
 {
   digitalWrite(25, LOW);
-  if(arm < 1350){
-  digitalWrite(24, HIGH);
-  delayMicroseconds(1000);    
+  if (arm < 1350) {
+    digitalWrite(24, HIGH);
+    delayMicroseconds(1000);
 
-  digitalWrite(24, LOW); 
-  delayMicroseconds(1000); 
+    digitalWrite(24, LOW);
+    delayMicroseconds(1000);
   }
- arm++; 
-}  
+  arm++;
+}
 void liftDown()
 {
   digitalWrite(25, HIGH);
-  if(arm < 1350){
-  digitalWrite(24, HIGH);
-  delayMicroseconds(1000);    
+  if (arm < 1350) {
+    digitalWrite(24, HIGH);
+    delayMicroseconds(1000);
 
-  digitalWrite(24, LOW); 
-  delayMicroseconds(1000); 
+    digitalWrite(24, LOW);
+    delayMicroseconds(1000);
   }
-  arm++; 
-  if(arm == 1350)
+  arm++;
+  if (arm == 1350)
   {
     dropFlag++;
   }
 
- 
-}  
-  
+
+}
+
